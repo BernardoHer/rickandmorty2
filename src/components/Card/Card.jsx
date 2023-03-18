ceb1a5c065830a5e8fc2e535ef2cada482df953b
BernardoHer <bernaher19@gmail.com>
2023-03-18 16:55:56 +0000

    Update 3 files

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 
 export default function Card(props) {
   return (
-    <Link to=''>
+    <Link to={`/detail/${props.id}`}>
     <div className={styles.card}>
       <CloseButton onClick={props.onClose} />
       <div className={styles.cardImage}>
--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
-export default function Detail(props){
-    return(
-        <>
-        
-        </>
-    )
-}
+export default function Detail(props) {
+  const { detailId } = useParams();
+  const [character, setCharacter] = useState({});
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${detailId}`)
+      .then((response) => response.json())
+      .then((char) => {
+        if (char.name) {
+          setCharacter(char);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      })
+      .catch((err) => {
+        window.alert("No hay personajes con ese ID");
+      });
+    return setCharacter({});
+  }, []);
+  return(
+   <div cl>
+       <h1 className={styles.h1} >Name: {character.name} </h1>
+       <h1 className={styles.h1} >Status: {character.status} </h1>
+       <h1 className={styles.h1} >Gender: {character.gender} </h1>
+       {/* <h1 className={styles.h1} >Origin:{character.origin} </h1> */}
+       <h1 className={styles.h1} >Image: {character.image} </h1>
+       
+       </div>
+  )}
