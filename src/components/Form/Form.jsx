37c494ff57bcf0485ab800de83455cfdbe93af11
BernardoHer <bernaher19@gmail.com>
2023-03-19 19:16:54 +0000

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import styles from "./Form.module.css";
 
 export default function Form(props) {
+  const [userData, setUserData] = useState({ username: '', password: '' });
+  const handleChange =(event) =>{
+    const {name,value} = event.target
+    setUserData({
+      ...userData,[name]: value,
+    })
+  }
+  useEffect(()=>{
+    console.log(userData)
+  },[userData])
+
   return (
     <form className={styles.container}>
         <img className={styles.img} src="https://wallpapercave.com/dwp1x/wp4791005.png" alt="not found"/>
       <label htmlFor="name" className={styles.label}>
         Nombre:
       </label>
-      <input type="text" id="name" className={styles.input} />
-      <label htmlFor="password" className={styles.label}>
+      <input onChange={handleChange} type="text" id="name" className={styles.input} value={userData.username} name='username'/>
+      <label   htmlFor="password" className={styles.label}>
         Password:
       </label>
-      <input type="password" id="password" className={styles.input} />
+      <input onChange={handleChange} type="password" id="password" className={styles.input}  value={userData.password} name='password'/>
       <button className={styles.button}>Login</button>
     </form>
   );
