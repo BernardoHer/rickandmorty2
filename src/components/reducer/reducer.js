942f8d936776db6cf302084b702aa06dd2520dfe
BernardoHer <109386038+BernardoHer@users.noreply.github.com>
2023-03-23 17:21:32 -0500

    ch

---
+++ src/components/reducer/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  // inicializa el estado
+};
+
+function reducer(state = initialState, action) {
+  // actualiza el estado en respuesta a las acciones
+}
+
+export default reducer;
